2eb47676a3faaa16621aab47668e436fb8f07b5a
SoftEngineerGabriel <farias2752@gmail.com>
2023-08-14 19:39:11 -0300

    Usando o query params

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-
+/* usando route  params*/
 app.get("/product/:id/:user", (request, response) => {
     const {id, user} = request.params;
     
@@ -12,7 +12,14 @@ app.get("/product/:id/:user", (request, response) => {
         Para o user: ${user}.
     `);    
 })
+/*usando query params*/
+app.get("/users", (request, response)=> {
+    const { page, limit} = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)
 )
+
