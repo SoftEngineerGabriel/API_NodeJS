8e20c01cedc958963c0138f1d38e619823ef6c26
SoftEngineerGabriel <farias2752@gmail.com>
2023-08-15 20:16:58 -0300

    AppError e trantando as exceções

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,6 +5,7 @@ const UsersController = require("../controllers/UsersControllers");
 
 const usersRoutes = Router();
 
+
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create)
