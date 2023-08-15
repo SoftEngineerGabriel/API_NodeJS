17319c22df2c671fcc655f5f91f1c35b18b9910d
SoftEngineerGabriel <farias2752@gmail.com>
2023-08-14 23:45:28 -0300

    Criacao do UserControllers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,12 @@
 const {Router} = require ("express");
+const UserController = require("../controllers/UsersControllers");
+
+const UsersController = require("../controllers/UsersControllers");
 
 const usersRoutes = Router();
 
+const usersController = new UsersController();
 
-usersRoutes.post("/", (request, response) => {
-    const {name, email , password} = request.body;
-
-    response.json({name, email, password});
-});
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes;
