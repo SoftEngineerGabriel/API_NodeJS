bacb024d7e6e55816086d5ccb2ea4d2198715b9f
SoftEngineerGabriel <farias2752@gmail.com>
2023-08-16 21:52:39 -0300

    Atualizando usuário

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -24,6 +24,36 @@ class UserController {
 
     }
 
+    async update(request, response){
+        const {name, email} = request.body;
+        const {id} = request.body;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado!")
+        }
+
+        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+            throw new AppError("Este e-mail já está em uso.")
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            update_at = ?
+            WHERE id = ?`, [user.name. user.email, new Date(), id]
+            );
+
+        return response.status(200).json();
+    }
 }
 
 module.exports = UserController;
