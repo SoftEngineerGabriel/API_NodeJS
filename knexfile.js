e25ea3cba136699b5fe586b067debaa76a1d2364
SoftEngineerGabriel <farias2752@gmail.com>
2023-08-18 19:20:48 -0300

    Migrations para link e tag

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(--dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+
+    pool:{
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreingn_keys = ON", cb)
+    },
+
+    migrations: {
+      directory : path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
   }
